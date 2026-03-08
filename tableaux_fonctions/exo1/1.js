272804f80cb77dc647e7418a67f5c344f2185dc1
Kiady06 <148479852+Kiady06@users.noreply.github.com>
2026-03-08 17:07:38 +0300

    yazzzy

--- tableaux_fonctions/exo1/1.js
+++ tableaux_fonctions/exo1/1.js
@@ -13,10 +13,27 @@ function getMinMaxIntger (tab) {
     return rslt;
 }
 
-let tab1 = [1,7,6,9,14,0];
-let tab2 = [0,0,0,0,0];
-let tab3 = [2,102,1,4,8,5,0,999,1];
+// let tab1 = [1,7,6,9,14,0];
+// let tab2 = [0,0,0,0,0];
+// let tab3 = [2,102,1,4,8,5,0,999,1];
 
-console.log(getMinMaxIntger(tab1));
-console.log(getMinMaxIntger(tab2));
-console.log(getMinMaxIntger(tab3));
+// console.log(getMinMaxIntger(tab1));
+// console.log(getMinMaxIntger(tab2));
+// console.log(getMinMaxIntger(tab3));
+const promptFunc = require(`prompt-sync`);
+const prompt = promptFunc();
+
+let tab = [];
+while (true) {
+    let input = prompt(`Input the number you want to add (end if over) : `);
+
+    if (input.trim().toLowerCase() === `end`) break;
+
+    if (!isNaN(Number(input))) {
+        tab.push(Number(input));
+    } else {
+        console.log (`Ce n'est pas un nombre valide (end pour finir)`)
+    }
+}
+
+console.log(getMinMaxIntger(tab));
